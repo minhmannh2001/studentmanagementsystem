c4120219a8f456401dea2971620a4c4bde99d03a
minhmannh2001 <nguyenminhmannh2001@gmail.com>
2022-06-15 17:32:29 +0700

    add video demo

--- add-challenges.php
+++ add-challenges.php
@@ -19,7 +19,7 @@
     }
 
     try {
-        $conn = new PDO("mysql:host=$db_servername;port=$db_port;dbname=$db_name", $db_username, $db_password);
+        $conn = new PDO("mysql:host=$db_servername;port=$db_port;dbname=$db_name", $db_username, $db_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
